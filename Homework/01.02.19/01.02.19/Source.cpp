1f831bb8cf4416bae33a0f634611afb9d7dd9d1a
ADMIN <ADMIN@AS>
2019-02-01 23:19:02 +0200

    Finished task

--- Homework/01.02.19/01.02.19/Source.cpp
+++ Homework/01.02.19/01.02.19/Source.cpp
@@ -7,68 +7,101 @@
 //    Знайти підсумкову оцінку кожного учасника(учасників 3), яка обчислюється як середнє арифметичне,
 //    але без врахування найкращої та найгіршої оцінок. Виконати задачу з функіями.
 
-const int PERSON = 3, POINTS = 10;
+const int NUMBER_PERSON = 3, NUMBER_POINTS = 10;
 
 void  Menu();
-void Points(int arr[PERSON][POINTS], const int PERSON, const int POINTS);
-void PrintResult(int arr[PERSON][POINTS], const int PERSON, const int POINTS);
-void Find(int arr[PERSON][POINTS], const int PERSON, const int POINTS);
+void Points(int arr[NUMBER_PERSON][NUMBER_POINTS]);
+void PrintResult(int arr[NUMBER_PERSON][NUMBER_POINTS]);
+void Find(int arr[NUMBER_PERSON][NUMBER_POINTS]);
 
 
 
 int main()
 {
-	int arr[PERSON][POINTS];
-
+	int arr[NUMBER_PERSON][NUMBER_POINTS];
+	srand(unsigned(time(NULL)));
 	Menu();
-	Points(arr, PERSON, POINTS);
-	PrintResult(arr, PERSON, POINTS);
+	Points(arr);
+	PrintResult(arr);
+	cout << "------------------------------------------------" << endl;
+	Find(arr);
 	system("pause");
 	return 0;
 }
 
 
 void Menu() {
-	cout << "   \"Welcome to our contest!!!\"\n\n Three participants take part in the contest!\n\n  Participant 1\n  Participant 2\n  Participant 3\n" << endl;
-	cout << "Judges!  Appreciate the participants!  " << endl;
+	cout << "   \"Welcome to our contest!!!\"\n\n Three participants take part in the contest!\n  " << endl;
+	cout << " Judges!  Appreciate the participants! \n " << endl;
 }
 
-void Points(int arr[PERSON][POINTS], const int PERSON, const int POINTS)
+void Points(int arr[NUMBER_PERSON][NUMBER_POINTS]) // Функція Points генерує очки учасників
 {
-	srand(unsigned(time(NULL)));
-	cout << "Participant's points: " << endl;
-	for (int i = 0; i < PERSON; ++i)
+	for (int i = 0; i < NUMBER_PERSON; ++i)
 	{
-		for (int j = 0; j < POINTS; ++j)
+		for (int j = 0; j < NUMBER_POINTS; ++j)
 		{
-			arr[i][j] = rand() % 10 + 1;;
+			arr[i][j] = rand() % 10 + 1;
 		}
 	}
 }
 
 
-void PrintResult(int arr[PERSON][POINTS], const int PERSON, const int POINTS)
+void PrintResult(int arr[NUMBER_PERSON][NUMBER_POINTS])  // Функція PrintResult виводить очки на екран
 {
-	cout << "Participant's points: " << endl;
-	for (int i = 0; i < PERSON; ++i)
+	cout << " Participant's points: \n" << endl;
+	for (int i = 0; i < NUMBER_PERSON; ++i)
 	{
-		for (int j = 0; j < POINTS; ++j)
+		for (int j = 0; j < NUMBER_POINTS; ++j)
 		{
-			cout <<"arr [" << i << "]" << " = " << arr[i][j] << endl;
+			cout <<" Participant " << i+1 << " = " << arr[i][j] << endl; // виведення результату (бали); i+1 - номер учасника змагань (+1 щоб 1го учасника виводило під номером 1);  arr[i][j] - бали учасника
 		}
 	}
 }
 
-
-void Find(int arr[PERSON][POINTS], const int PERSON, const int POINTS)
-{
-	cout << "Participant's points: " << endl;
-	for (int i = 0; i < PERSON; ++i)
-	{
-		for (int j = 0; j < POINTS; ++j)
-		{
-			
+int Max(int arr[NUMBER_PERSON][NUMBER_POINTS], int i) {  //   Ця функція визначає максимальний бал; (int arr[NUMBER_PERSON][NUMBER_POINTS], int i) - параметри функції, які вона приймає;
+	int max = 0;
+	int MaxNum = 0;
+	for (int j = 0; j < NUMBER_POINTS; j++) { // проходимось по колонках масиву щоб визначити максимальне очко
+		if (max < arr[i][j]) {
+			max = arr[i][j];
+			MaxNum = max;
 		}
 	}
+	return MaxNum;
+}
+
+int Min(int arr[NUMBER_PERSON][NUMBER_POINTS], int i) {   //   Ця функція визначає мінімальний бал; (int arr[NUMBER_PERSON][NUMBER_POINTS], int i) - параметри функції, які вона приймає;
+	int min = 0;
+	int MinNum = 0;
+	for (int j = 0; j < NUMBER_POINTS; j++) { // проходимось по колонках масиву щоб визначити мінімальне очко 
+		if (min > arr[i][j]) {
+			min = arr[i][j];
+			MinNum = min;
+		}
+	}
+	return MinNum;
+}
+
+void Find(int arr[NUMBER_PERSON][NUMBER_POINTS]) //Ця ф-ція знаходить значення всіх очків без урахування Макс і Мін значення 
+{
+	int counter = 0;
+	int maxNum;
+	int minNum;
+	for (int i = 0; i < NUMBER_PERSON; ++i)
+	{
+		counter = 0; // пишемо у циклі для того, щоб після підрахування загального результату кожного учасника бали обнулялися перед підрахуванням балів наступного учасника (без цього враховує бали попередніх учасників)
+		maxNum = Max(arr, i);  // викликаємо ф-цію Max (Max(arr, i) - це функція ((arr,i) параметри функції з якими ми будемо працювати))
+		minNum = Min(arr, i);  // викликаємо ф-цію Min (Min(arr, i) - це функція ((arr,i) параметри функції з якими ми будемо працювати))
+		for (int j = 0; j < NUMBER_POINTS; ++j)
+		{	
+			if (j != maxNum && j != minNum)
+			{
+				counter += arr[i][j];
+			}
+		}
+		cout << "Average amount of points for \" Participant " << i + 1 << " \" = " << counter / 8 << endl;
+	}
+	cout << endl;
 }
 
