a08f18a7ac2470cc3004c4ee81c2b8edce1d23ad
Nataliia Fedorovych <natashaum3177@gmail.com>
2019-02-12 11:54:48 +0200

    Class work

---
+++ Class work/07.02.19/07.02.19/Source.cpp
@@ -0,0 +1,78 @@
+﻿/*#include <iostream>
+using namespace std;
+   
+template <typename T1, typename T2>
+void Sum(T1 a, T2 b) {
+	cout << a << endl;
+	cout << b << endl;
+}
+int main() {
+	Sum(5, 6);
+	Sum(5.8, 2.1);
+	Sum("Bill", "Vasil");
+	Sum(5, 8.5);
+	Sum(8.5, 5);
+	system("pause");
+	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+
+template <typename T1, typename T2>
+T1 Sum(T1 a, T2 b) {
+	return a + b;
+}
+
+
+template <typename T1, typename T2>
+T2 Sum(T1 a, T2 b, T1 c) {
+	return a + b + c;
+}
+int main() {
+	cout << Sum (5.5, 8) << endl;
+	cout << Sum (5, 5.2, 100) << endl;
+	system("pause");
+	return 0;
+}*/
+
+/*1. Написати наступні функції, які в якості параметра приймають двохвимірний масив і його розмірності.
+     Перевірити роботу функції для двохвимірних масивів розмірністю 3х4 та 7х5.
+     функцію Fill(), яка заповнює двохвимірний масив випадковими значеннями в діапазоні[1..30]
+     шаблонну функцію Print(), яка виводить елементи масиву на екран в вигляді матриці
+     функцію, яка заміняє всі парні елементи нулями  */
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+const int Row = 3;
+const int Col = 4;
+
+void FillArray(int arr[Row][Col], const int Row, const int Col) {
+	for (int i = 0; i < Row; i++) {
+		for (int j = 0; j < Col; j++) {
+	        arr[i][j] = rand() % 30 + 1;
+		}
+	}
+}
+
+void PrintArray(int arr[Row][Col], const int Row, const int Col) {
+	for (int i = 0; i < Row; i++) {
+		for (int j = 0; j < Col; j++) {
+			cout << arr[i][j] <<"   ";
+		}
+	}
+}
+
+int main() {
+	srand(unsigned(time(NULL)));
+	int arr[Row][Col];
+	FillArray(arr, Row, Col);
+	PrintArray(arr, Row, Col);
+	//cout << arr[Row][Col] << "  " <<endl;
+	system("pause");
+	return 0;
+}
+
+
---
+++ Class work/12.02.19/12.02.19/Source.cpp
@@ -0,0 +1,104 @@
+﻿//#include <iostream>
+//using namespace std;
+////void Foo();
+////void Foo(int counter);
+//int Foo(int counter);
+//int i = 0;
+//
+//int main() {
+//	//Foo(10);
+//	//Foo();
+//	Foo(10); // в () пишемо скільки раз нам потрібно викликати певну ф-ю;
+//	system("pause");
+//	return 0;
+//}
+///*void Foo() {   // перед тим як викликати цю ф-ю, треба оголосити глобальне "і", і викликати ф-ю в мейні;
+//	cout << "Foo" << i++ << endl;
+//	Foo(); // рекурсивна функція, яка викликає сама себе поки не закінчиться виділена для неї пам'ять;
+//}*/
+///*void Foo(int counter) {
+//	counter--;//віднімається поки каунтер буде більший 0
+//	cout << "Foo counter " << counter << endl;
+//	if (counter > 0) {
+//		Foo(counter);
+//	}
+//}*/
+//int Foo(int counter) {
+//	if (counter < 1) {
+//		return 0;
+//	}
+//	counter--;
+//	cout << "int Foo  " << counter<< endl; 
+//	return Foo(counter);
+//}*/
+
+// Написати функцію, яка рекурсивно обчислює суму чисел в переданому діапазоні
+
+/*int sum = 0;
+
+int Sum(int a, int b){
+	
+	cout << a << " = " << b << endl;
+	sum += a;
+	if (a == b) {
+		return sum;
+	}
+
+	return Sum(a + 1, b);
+
+}
+int main() {
+	int rezult = Sum(5, 10);
+	cout << rezult << endl;
+	system("pause");
+	return 0;
+}*/
+
+// Написати шаблон функцію для роботи з масивом:
+// a. (або заповнення випадковими числами)  масиву
+// b. виведення масиву на екран
+// c. реалізувати можливість сортування масиву(зростання.спадання) по вибору користувача(вибір здійснюється за допомогою типу bool)
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+template <typename T>
+void FillArr(T arr) {
+	for (int i = 0; i < 10; i++)
+		arr[i] = rand() % 10 + 1;
+}
+template <typename T>
+void PrintArr(T arr) {
+	for (int i = 0; i < 10; i++) {
+		cout << "Arr " << i << "   " << arr[i] << endl;
+	}
+	cout << "========================================>" << endl;
+}
+void Menu() {
+	cout << " Choose the way you want sort the arrey: " << endl;
+	cout << " Choose 1 to Sort Down " << endl;
+	cout << " Choose 2 to Sort Up " << endl;
+}
+
+//void SortDown(int arr[], int n)
+//{
+//	
+//}
+
+int main() {
+	srand(unsigned(time(NULL)));
+
+	int arr[10];
+	float arr[10];
+	FillArr(arr);
+	PrintArr(arr);
+	float arr2[10];
+	FillArr(arr2);
+	PrintArr(arr2);
+	Menu();
+
+	system("pause");
+	return 0;
+}
