afc54418173f848c0ab00f62b2741812ee0ede13
Natashaum <natashaum3177@gmail.com>
2019-02-20 23:18:08 +0200

    Completed Homework 19.02.19 (3 tasks)

--- Homework/19.02.19/Source.cpp
+++ Homework/19.02.19/Source.cpp
@@ -71,3 +71,134 @@ int main() {
 	system("pause");
 	return 0;
 }*/
+
+//  3.	Написать примитивный калькулятор, пользуясь только указателями.
+
+int Menu() {
+	int choice = 0;
+	cout << "  ***  Choose the action you need to do: ***" << endl;
+	cout << "----------------------------------------------" << endl;
+	cout << "1. Addition        \"+\" \n2. Subtraction     \"-\"\n3. Multiplication  \"*\"\n4. Division        \"/\"\n5. Exit!" << endl << endl;
+
+	cin >> choice;
+	return choice;
+}
+//void Sum( int *pA, int *pB, int *pS) {
+//	cout << "Enter the first number: " << endl;
+//	cin >> *pA;
+//	cout << "Enter the secont number: " << endl;
+//	cin >> *pB;
+//	*pS = *pA + *pB;
+//}
+void Sum(double* pA, double* pB, double* pS) {
+	cout << "Enter the first number: " << endl;
+	cin >> *pA;
+	cout << "Enter the secont number: " << endl;
+	cin >> *pB;
+	*pS = *pA + *pB;
+}
+
+//void Risn(int* pA, int* pB, int* pR) {
+//	cout << "Enter the first number: " << endl;
+//	cin >> *pA;
+//	cout << "Enter the secont number: " << endl;
+//	cin >> *pB;
+//	*pR = *pA -*pB;
+//}
+void Risn(double* pA, double* pB, double* pR) {
+	cout << "Enter the first number: " << endl;
+	cin >> *pA;
+	cout << "Enter the secont number: " << endl;
+	cin >> *pB;
+	*pR = *pA - *pB;
+}
+
+//void Dob(int* pA, int* pB, int* pD) {
+//	cout << "Enter the first number: " << endl;
+//	cin >> *pA;
+//	cout << "Enter the secont number: " << endl;
+//	cin >> *pB;
+//	*pD = *pA * *pB;
+//}
+void Dob(double* pA, double* pB, double* pD) {
+	cout << "Enter the first number: " << endl;
+	cin >> *pA;
+	cout << "Enter the secont number: " << endl;
+	cin >> *pB;
+	*pD = *pA * *pB;
+}
+
+//void Chast(int* pA, int* pB, int* pCh) {
+//	cout << "Enter the first number: " << endl;
+//	cin >> *pA;
+//	cout << "Enter the secont number: " << endl;
+//	cin >> *pB;
+//	*pCh = *pA / *pB;
+//}
+void Chast(double *pA, double *pB, double *pCh) {
+	cout << "Enter the first number: " << endl;
+	cin >> *pA;
+	cout << "Enter the secont number: " << endl;
+	cin >> *pB;
+	*pCh = *pA / *pB;
+}
+
+
+int main() {
+	double dob = 0;
+	double sum = 0;
+	double risn = 0;
+	double chast = 0;
+	double a = 0;
+	double b = 0; 
+	double* pA = &a;
+	double* pB = &b;
+	double* pS = &sum;
+	double* pR = &risn;
+	double* pD = &dob;
+	double* pCh = &chast;
+	
+
+
+	for (;;) {
+		int choice = Menu();
+
+		if (choice == 1) {
+			cout << " You want to Add (+) numbers!" << endl;
+			cout << "------------------------------" << endl;
+			Sum(pA, pB, pS);
+			cout << "Sum = " << sum <<endl;
+		}
+		else if (choice == 2) {
+			cout << " You want to Subtract (-) numbers!" << endl;
+			cout << "-----------------------------------" << endl;
+			Risn (pA, pB, pR);
+			cout << "Risnutsia = " << risn << endl;
+		}
+		else if (choice == 3) {
+			cout << " You want to Multiply (*) numbers!" << endl;
+			cout << "-----------------------------------" << endl;
+			Dob(pA, pB, pD);
+			cout << "Dobutok = " << dob << endl;
+		}
+		else if (choice == 4) {
+			cout << " You want to Divide (/) numbers!" << endl;
+			cout << "-----------------------------------" << endl;
+			Chast(pA, pB, pCh);
+			cout << "Chastka = " << chast << endl;
+		}
+		else if (choice == 5) {
+			cout << "  \" See you! \"\n \" Good Luck!  ;)\"" << endl << endl;
+			break;
+		}
+		else {
+			cout << "\" You've chosen wrong number! \"\n \" Press any key to try again \" " << endl;
+		}
+
+		system("pause");
+		system("cls");
+	}
+	system("pause");
+	return 0;
+}
+
