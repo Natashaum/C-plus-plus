2f856ebfebe74ef4795b32e71b6942bb83efd4b8
Nataliia Fedorovych <Fedorovych@rivne.itstep.org>
2019-04-16 11:54:30 +0300

    Memory for bit  16.04.19

---
+++ Class work/06.03.19/07.03.19/Source.cpp
@@ -0,0 +1,102 @@
+﻿//  1. Написати функції для роботи з динамічним одновимірним масивом :
+//     Функція створення динамічного масиву вказаного розміру і його заповнення випадковими числами.
+//     Виводу масиву
+//     Доповнення масиву одним елементом в кінець масиву.
+//     Доповнення масиву одним елементом в початокмасиву.
+//     Меню
+
+
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+void Fill(int* const arr, const int size) {
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 10;
+	}
+}
+void Print(int* const arr, const int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << "\t";
+	}
+}
+void Push(int*& arr, int size, int value) {  // * &arr  - вказівник на посилання //  ф-я для додавання останнього елементу;
+	int* newarr = new int[size + 1];
+	for (int i = 0; i < size; i++) {
+		newarr[i] = arr[i];
+	}
+	newarr[size] = value;
+	delete[]arr;
+	arr = newarr;
+}
+void Push1(int*& arr, int size, int value) { //  ф-я для додавання першого елементу;
+	int* newarr = new int[size + 1];
+
+	newarr[0] = value;
+	for (int i = 1; i < size + 1; i++) {
+		newarr[i] = arr[i - 1];
+	}
+
+	delete[]arr;
+	arr = newarr;
+}
+
+void Menu(int*& arr, int& size) {
+
+	cout << " Make your choice:\n Press:\n 1 - to Fiil and Print Array\n 2 - to Add Last element\n 3 - to Add First element\n 4 - to Exit!\n";
+	cout << "-----------------------------------------------------" << endl;
+	for (;;) {
+		cout << "Your choice is  ";
+		int choice = 0;
+		cin >> choice;
+		if (choice == 1) {
+			Fill(arr, size);
+			Print(arr, size);
+			cout << endl;
+			cout << "=====================================================" << endl;
+		}
+		else if (choice == 2) {
+			cout << "Enter last element " << endl;
+			int element;
+			cin >> element;
+			Fill(arr, size);
+			Push(arr, size, element);
+			size++;   // щоб збільшити змінну на розмір масиву;
+			Print(arr, size);
+			cout << endl;
+			cout << "=====================================================" << endl;
+		}
+		else if (choice == 3) {
+			cout << "Enter first element " << endl;
+			int element;
+			cin >> element;
+			Fill(arr, size);
+			Push1(arr, size, element);
+			size++;   // щоб збільшити змінну  size  на розмір масиву;
+			Print(arr, size);
+			cout << endl;
+			cout << "=====================================================" << endl;
+		}
+		else if (choice == 4) {
+			cout << " Exit!" << endl;
+			break;
+		}
+		else {
+			cout << " You have done incorrect choice!!! \n      Try again! " << endl;
+			cout << "==============================================" << endl;
+		}
+	}
+}
+int main() {
+	int size = 5;
+	int* arr = new int[size];
+
+	Menu(arr, size);
+
+
+	delete[]arr;
+	system("pause");
+	return 0;
+}
---
+++ Class work/16.04.19/16.04.19/Source.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+/*struct Person {
+	string name;
+	unsigned int age : 8;  // unsigned - для того щоб число не було від'ємним // age : 8  (виділяємо лише 8 біт, щоб не займало більше ніж нам потрібно пам'яті в програмі);
+};
+int main() {
+	Person Bill;
+	Bill.name = "Bill";
+	Bill.age = 256;
+	cout << "Name:   " << Bill.name << "\nAge:   " << Bill.age << endl;  // якщо не в структурі, то треба писати Bill.name і т.д.*/
+//====================================================================================================================================
+//====================================================================================================================================
+
+// Створити структуру з бітовими полями Час(години.хвилини, секунди).Продумати яка мінімальна кількість біт потрібна для збереження певного  елемента структури.
+// Визначити функції :заповнення об’єкта структури з клавіатури
+// вивід  об’єкта структури  на екран
+struct Time {
+	unsigned int hours : 6;
+	unsigned int minutes : 7;
+	unsigned int seconds : 7;
+};
+
+int main(){
+	int minute = 0;
+	int hour = 0;
+	int second = 0;
+	cout << " Enter hours:     " ;
+	cin >> hour;
+	cout << " Enter minutes:   " ;
+	cin >> minute;
+	cout << " Enter seconds:   " ;
+	cin >> second;
+	Time Bit;
+	Bit.hours = hour;
+	Bit.minutes = minute;
+	Bit.seconds = second;
+
+	cout << "Hours:      " << Bit.hours << "\nMinutes:    " << Bit.minutes << "\nSeconds:    " << Bit.seconds << endl;
+
+
+	system("pause");
+	return 0;
+}
