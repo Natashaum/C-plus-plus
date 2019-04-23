26c8401215e1254a8fbbe23fff1b5b88f349dea8
Nataliia Fedorovych <Fedorovych@rivne.itstep.org>
2019-04-23 11:52:36 +0300

    OVP 23.04.19

---
+++ Class work/OVP intro 23.04.19/OVP intro 23.04.19/Source.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+//класи пишемо за межами інта
+/*class car {
+public:
+	string model;
+	float power;
+	string colour;
+	int weight;
+	string RegNumber;
+
+	void ShowCarInfo() {  // маємо робити в середині класу
+		cout << "Model:    " << model << "\nPower:    " << power << "\nWeight:   " << weight << "\nRegNumber:" << RegNumber << endl;  // замість cout в int
+		
+	}
+};
+
+
+int main() {
+	cout << "Mersedes" << "\n-------------------------------------------" << endl;
+	car Mersedes;  //car - клас, Mersedes - об'єкт класу
+	Mersedes.model = "A 140";
+	Mersedes.power = 1.6;
+	Mersedes.weight =1500;
+	Mersedes.RegNumber = "1200 AA";
+	Mersedes.ShowCarInfo();
+	////cout << "Model:    " << Mersedes.model << "\nPower:    " << Mersedes.power << "\nWeight:   " << Mersedes.weight << "\nRegNumber:" << Mersedes.RegNumber << endl;
+	cout << "============================================" << endl;
+	cout << "Mersedes" << "\n-------------------------------------------" << endl;
+	Mersedes.model = "280"; //при зміні даних, виводиться на екран нові дані 
+
+	Mersedes.ShowCarInfo();
+	////cout << "Model:    " << Mersedes.model << endl;
+	
+	cout << "=============================================" << endl;
+	cout << "BMV" << "\n-------------------------------------------" << endl;
+	car BMV;  //car - клас, Mersedes - об'єкт класу
+	BMV.model = "A 150";
+	BMV.power = 2.5;
+	BMV.weight = 2000;
+	BMV.RegNumber = "1200 BA";
+	BMV.ShowCarInfo();
+	
+	cout << "=============================================" << endl;
+	cout << "BMV" << "\n-------------------------------------------" << endl;
+	BMV.power = 3.1;
+	BMV.ShowCarInfo();
+	cout << "=============================================" << endl;
+	system("pause");
+	return 0;
+}*/
+
+//ООП
+
+//Створити клас "Точка" (Point), який складається з двох полів типу int: X та Y.Необхідно забезпечити :
+//Введення координат точки користувачем
+//Вивід інформації про точку на екран
+//Можливість зміни будь - якої з координат на запит користувача(X або Y)
+/*class Points {
+public:
+	int x;
+	int y;
+
+	void ShowPointInfo() {
+		cout << x << " \n" << y << endl;
+	}
+};
+int main() {
+	Points coordinates; //Points - клас, coordinates - об'єкт класу
+	int x = 0; //для вводу даних користувачем
+	int y = 0; //для вводу даних користувачем
+	cout << "Enter Point x:  ";
+	cin >> x;
+	cout << "Enter Point y:  ";
+	cin >> y;
+	coordinates.x = x;
+	coordinates.y = y;
+	coordinates.ShowPointInfo();
+	system("pause");
+	return 0;
+}*/
+
+/*class Car {
+private:
+	string model;
+	float power;
+	string engine;
+	int year;
+
+public:
+	void ShowCarInfo() {
+		cout << "Model:  " << model << "\nPower: " << power << "\nEngine" << year << endl;
+	}
+	void SetModel(string newModel) {  //щоб доступитися з int main до моделі (ф-я приймає параметри моделі)
+		model = newModel;
+	}
+	string GetModel() {
+		return model;
+	}
+};
+int main() {
+	Car mazda;
+	mazda.SetModel("Mazda");
+	mazda.ShowCarInfo();
+
+	cout <<  "From GetModel function:   " << mazda.GetModel() << endl;
+
+	mazda.SetModel("Mazda 3"); //в дужках задаємо параметри
+	cout << "From GetModel function:   " << mazda.GetModel() << endl; // для виводу на екран
+
+	system("pause");
+	return 0;
+}*/
+
---
+++ Homework/OVP 23.04.19/OVP 23.04.19/Source.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+/*class car {
+public:
+	string model;
+	float power;
+	string colour;
+	int weight;
+	string RegNumber;
+
+	void ShowCarInfo() {  // маємо робити в середині класу
+		cout << "Model:    " << model << "\nPower:    " << power << "\nWeight:   " << weight << "\nRegNumber:" << RegNumber << endl;  // замість cout в int
+
+	}
+};
+
+
+int main() {
+	cout << "Mersedes" << "\n-------------------------------------------" << endl;
+	car Mersedes;  //car - клас, Mersedes - об'єкт класу
+	Mersedes.model = "A 140";
+	Mersedes.power = 1.6;
+	Mersedes.weight =1500;
+	Mersedes.RegNumber = "1200 AA";
+	Mersedes.ShowCarInfo();
+	////cout << "Model:    " << Mersedes.model << "\nPower:    " << Mersedes.power << "\nWeight:   " << Mersedes.weight << "\nRegNumber:" << Mersedes.RegNumber << endl;
+	cout << "============================================" << endl;
+	cout << "Mersedes" << "\n-------------------------------------------" << endl;
+	Mersedes.model = "280"; //при зміні даних, виводиться на екран нові дані
+
+	Mersedes.ShowCarInfo();
+	////cout << "Model:    " << Mersedes.model << endl;
+
+	cout << "=============================================" << endl;
+	cout << "BMV" << "\n-------------------------------------------" << endl;
+	car BMV;  //car - клас, Mersedes - об'єкт класу
+	BMV.model = "A 150";
+	BMV.power = 2.5;
+	BMV.weight = 2000;
+	BMV.RegNumber = "1200 BA";
+	BMV.ShowCarInfo();
+
+	cout << "=============================================" << endl;
+	cout << "BMV" << "\n-------------------------------------------" << endl;
+	BMV.power = 3.1;
+	BMV.ShowCarInfo();
+	cout << "=============================================" << endl;
+	system("pause");
+	return 0;
+}*/
+
+
+//ООП
+//Завдання 2. Домашнє завдання
+//Написати клас "Банківський рахунок" (Account), який містить :
+//Номер рахунку
+//Розмір коштів на рахунку
+//Назва валюти рахунку(рублі, гривні, евро тощо), для позначення якої можна скористатись одним символом : R, G, E, $ тощо
+//Забезпечити можливість :
+//Відкривати рахунок та первинно вносити гроші на рахунок
+//Знімати гроші з рахунку
+//Докладати гроші на рахунок
+class BankAccount {
+public:
+	int accountNumber;
+	string userName;
+	string userSurname;
+	int sumMoney;
+	string moneyName;
+	void ShowInfo() {
+		cout << "Account number:        " << accountNumber << "\nUser name:             " << userName << "\nUser surname:          " << userSurname <<"\nAmount of money:       " << sumMoney << "\nThe name of currency:  " << moneyName << endl;
+	}
+};
+
+
+
+int main() {
+	BankAccount account;
+	account.accountNumber = 555777888;
+	account.userName = "Anna";
+	account.userSurname = "Glain";
+	account.sumMoney = 10500;
+	account.moneyName = "HRN";
+	account.ShowInfo();
+
+	BankAccount account;
+	account.accountNumber = 111333222;
+	account.userName = "Tom";
+	account.userSurname = "Tomson";
+	account.sumMoney = 5000;
+	account.moneyName = "$";
+	account.ShowInfo();
+
+	
+	system("pause");
+	return 0;
+}
